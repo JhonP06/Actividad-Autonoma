a79d99fd8ab19ff7a8295ed3196c6e5c7043b519
Jhon Pillisa <jhonalexpillisa@gmail.com>
2024-07-31 15:27:15 -0500

    cambios finales

--- src/Components/Arreglo.tsx
+++ src/Components/Arreglo.tsx
@@ -1,13 +1,19 @@
-import { useState } from "react"
+import { count } from 'console';
+import { useState} from 'react';
 
 export const Arreglo = () => {
     const numeros: number[]=[5,10,15,20,25,30,35,40,45,50]
-    const cadena = numeros.map((num) => num /5);
+    const cadena = numeros.map((valorActual: number, index:number, array:number[]) => {
+
+    const valorDiv = valorActual / 5
+    return valorDiv 
+    })
+    
 
   return (
     <div>
         <h3>3.Del siguiente arreglo devolver un nuevo arreglo con los valores divididos para 5.</h3>
-        
+        console.log('Valor divisible', valorMulti) 
     </div>
   )
 }
